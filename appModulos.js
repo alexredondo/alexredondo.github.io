0eedcf4ca2f7ae9b9ae37a1e1ce52400282dfaab
Alexander Redondo <alexander.red@gmail.com>
2020-04-23 18:30:14 -0500

    Versión Mejorada 23042020
    Se organiza el código. Se usa una sola sheet.

---
+++ appModulos.js
@@ -0,0 +1,64 @@
+// Sirve a las páginas de los cursos
+// BOTONERA DE CURSOS SUPERIOR
+    
+    document.getElementById("botoneraCursosSuperior").innerHTML = `
+
+        
+        <div class="card-body">
+            <a href="parvulo.html" class="badge badge-primary">🐹 Párvulo</a>
+            <a href="prejardinA.html" class="badge badge-success">🐰A Prejardín</a>
+            <a href="prejardinB.html" class="badge badge-danger">🐰B Prejardín</a>
+            <a href="jardinA.html" class="badge badge-warning">🐻A Jardín</a>
+            <a href="jardinB.html" class="badge badge-info">🐻B Jardín</a>
+            <a href="transicionA.html" class="badge badge-info colorAltCoral">🐼A Transición</a>
+            <a href="transicionB.html" class="badge badge-info colorAltmediumslateblue">🐼B Transición</a>
+        </div>   
+        `
+;
+    document.getElementById("aviso").innerHTML = `
+ 
+            <div class="card-header">
+                Estimadas familias !
+            </div>
+            <div class="card-body">  
+            <blockquote class="blockquote mb-0">
+                    <p>Usted y su familia hace realidad este proyecto. <br><br> No hay palabras para agradecer tanto apoyo. <br><br> Gracias por ser valiente y superar el miedo. <br><br> Gracias por no dejarnos.</p>
+                    <footer class="blockquote-footer">Alex Redondo <cite title="Source Title">Director</cite></footer>
+            </blockquote>
+        `
+    ;
+// NAV BOTTOM
+    document.getElementById("navBottom").innerHTML = `
+    <a class="navbar-brand" href="#perfilEstudiante"><span class="iconoNavBar borderIcono">👤</span></a>
+    <a class="navbar-brand puffOut" href="#seguimientoEstudiante"><span class="iconoNavBar borderIcono ">📑<span></a>
+    <a class="navbar-brand" href="#registro_pagos"><span class="iconoNavBar borderIcono">💵<span></a>
+    <a class="navbar-brand" href="Tel:573017495229"><span class="iconoNavBar borderIcono">📞<span></a>
+    `
+;
+// START FOOT WEB -->
+
+document.getElementById("navJumbotron").innerHTML = `
+    <div class="container">
+        <h1 class="display-4">Te escuchamos...</h1>
+        <p class="lead">Estamos disponibles para atender sus solicitudes. No dude en usar cualquiera de los canales para tener el gusto de atenderlo.</p>
+
+        <addres class="lead">
+        Director <a href="mailto:alexredondo@academia.net.co">Esp. Alexander Redondo F.</a><br>
+        Carrera 28 No. 10-45 Barrio Puerta Roja<br>
+        Sincelejo, Sucre, Colombia (CO)<br>
+        Código Postal 70001<br>
+        </address>
+            <a href="Tel:5752743450" ></a>☎ 
+                Tel&eacute;fono : (+57) 52720074
+        </a><br> 
+            
+        <ul class="list-group list-group-horizontal listUlCell">
+            <li class="list-group-item listCell"><a href="Tel:573017495229" >📱</a></li>
+            <li class="list-group-item listCell"><a href="Tel:573017495229" >Celular : (+57) 301 7495229</a></li>
+            <li class="list-group-item listCell"><a href="Tel:573002835959" >Celular : (+57) 300 2835959</a></li>
+            <li class="list-group-item listCell"><a href="Tel:573022999720" >Celular : (+57) 302 2999720</a></li>
+        </ul>	
+        </div>
+    </div>
+
+`
